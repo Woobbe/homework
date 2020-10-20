8d792ea9f20f10f82c3873789dc168c134ab62e2
Vitalii Shevtsov <woobbe@gmail.com>
2020-10-20 22:40:08 +0300

    Task_2 Done

--- main_2.js
+++ main_2.js
@@ -1,67 +1,80 @@
-
 const listStudents = {
     student1: {
         name: 'Vasya',
         startYear: 2000,
         endYear: 2005
-    },student2: {
+    }, student2: {
         name: 'Petya',
         startYear: 2001,
         endYear: 2006
-    },student3: {
+    }, student3: {
         name: 'Alex',
         startYear: 1999,
         endYear: 2004
-    },student4: {
+    }, student4: {
         name: 'Oleg',
         startYear: 2000,
-        endYear: 2001
-    },student5: {
+        endYear: 2002
+    }, student5: {
         name: 'Igor',
-        startYear: 2007,
+        startYear: 2006,
         endYear: 2009
-    },student6: {
+    }, student6: {
         name: 'Anna',
         startYear: 2000,
-        endYear: 2003
-    },student7: {
+        endYear: 2002
+    }, student7: {
         name: 'Valeria',
         startYear: 2001,
         endYear: 2006
-    },student8: {
+    }, student8: {
         name: 'Kate',
-        startYear: 2002,
+        startYear: 2001,
         endYear: 2007
-    },student9: {
+    }, student9: {
         name: 'Maria',
         startYear: 2002,
         endYear: 2005
-    },student10: {
+    }, student10: {
         name: 'Victoria',
         startYear: 2000,
         endYear: 2001
-    },showStudents: function(startYearFilter, endYearFilter) {
+    }, showStudents: function(startYearFilter, endYearFilter) {
+
         let listYears = [];
         let result = {};
         let count = 0;
+        let maxYear = [];
 
         for (startYearFilter; startYearFilter <= endYearFilter; startYearFilter++) {
             listYears = [];
+
             for (let student in this) {
+
                 if (startYearFilter >= this[student].startYear && startYearFilter <= this[student].endYear) {
                     listYears.push(startYearFilter);
                 }
             }
+
             result[startYearFilter] = listYears.length;
         }
+
         for (let year in result) {
+
+            if (count === result[year]) {
+                maxYear.push(year);
+            }
+
             if (count < result[year]) {
                 count = result[year];
+                maxYear = [year];
             }
         }
-        console.log(result);
-    }
+
+    return console.log(`Максимальное количество студентов было в ${maxYear} году - ${count} студентов.`);
+}
 
 };
+
 
 listStudents.showStudents(2000, 2009);
