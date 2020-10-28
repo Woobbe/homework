eb0a3fec3056729f5db3afad0c692e2fe64c8e14
Vitalii Shevtsov <woobbe@gmail.com>
2020-10-28 20:06:32 +0200

    edit style obj

--- main_1.js
+++ main_1.js
@@ -3,43 +3,53 @@ const listStudents = {
         name: 'Vasya',
         startYear: 2000,
         endYear: 2005
-    }, student2: {
+    },
+    student2: {
         name: 'Petya',
         startYear: 2001,
         endYear: 2006
-    }, student3: {
+    },
+    student3: {
         name: 'Alex',
         startYear: 1999,
         endYear: 2004
-    }, student4: {
+    },
+    student4: {
         name: 'Oleg',
         startYear: 2000,
         endYear: 2001
-    }, student5: {
+    },
+    student5: {
         name: 'Igor',
         startYear: 2007,
         endYear: 2009
-    }, student6: {
+    },
+    student6: {
         name: 'Anna',
         startYear: 2000,
         endYear: 2003
-    }, student7: {
+    },
+    student7: {
         name: 'Valeria',
         startYear: 2001,
         endYear: 2006
-    }, student8: {
+    },
+    student8: {
         name: 'Kate',
         startYear: 2002,
         endYear: 2007
-    }, student9: {
+    },
+    student9: {
         name: 'Maria',
         startYear: 2002,
         endYear: 2005
-    }, student10: {
+    },
+    student10: {
         name: 'Victoria',
         startYear: 2000,
         endYear: 2001
-    }, showStudents: function(startYearFilter, endYearFilter) {
+    },
+    showStudents: function(startYearFilter, endYearFilter) {
         let result = [];
 
         for (let student in this) {
--- main_2.js
+++ main_2.js
@@ -3,43 +3,53 @@ const listStudents = {
         name: 'Vasya',
         startYear: 2000,
         endYear: 2005
-    }, student2: {
+    },
+    student2: {
         name: 'Petya',
         startYear: 2001,
         endYear: 2006
-    }, student3: {
+    },
+    student3: {
         name: 'Alex',
         startYear: 1999,
         endYear: 2004
-    }, student4: {
+    },
+    student4: {
         name: 'Oleg',
         startYear: 2000,
         endYear: 2002
-    }, student5: {
+    },
+    student5: {
         name: 'Igor',
         startYear: 2006,
         endYear: 2009
-    }, student6: {
+    },
+    student6: {
         name: 'Anna',
         startYear: 2000,
         endYear: 2002
-    }, student7: {
+    },
+    student7: {
         name: 'Valeria',
         startYear: 2001,
         endYear: 2006
-    }, student8: {
+    },
+    student8: {
         name: 'Kate',
         startYear: 2001,
         endYear: 2007
-    }, student9: {
+    },
+    student9: {
         name: 'Maria',
         startYear: 2002,
         endYear: 2005
-    }, student10: {
+    },
+    student10: {
         name: 'Victoria',
         startYear: 2000,
         endYear: 2001
-    }, showStudents: function(startYearFilter, endYearFilter) {
+    },
+    showStudents: function(startYearFilter, endYearFilter) {
         let listYears = [];
         let result = {};
         let count = 0;
