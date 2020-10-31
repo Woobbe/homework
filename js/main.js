054c0a99ed3232720d2d1c4033b91a16b13a4876
Vitalii Shevtsov <woobbe@gmail.com>
2020-10-31 22:49:03 +0200

    fixed changes 99%

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function showDropdownMenu() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById('myDropdown').classList.toggle('show');
 }
 
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
 
-        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let dropdowns = document.getElementsByClassName('dropdown-content');
         for (let i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
@@ -13,13 +13,13 @@ window.onclick = function(event) {
             }
         }
     }
-}
-
+};
 
 function hideAside() {
-document.querySelector('.aside').classList.toggle('aside-hide');
-document.querySelector('.page__state').classList.toggle('page__state-all_width');
-document.querySelector('.aside__content').classList.toggle('aside__content-hide');
-document.querySelector('.hamburger-menu').classList.toggle('hamburger-menu-left')
+    document.querySelector('.aside').classList.toggle('aside-hide');
+    document.querySelector('.page__state').classList.toggle('page__state-all_width');
+    document.querySelector('.aside__content').classList.toggle('aside__content-hide');
+    document.querySelector('.hamburger-menu').classList.toggle('hamburger-menu-left');
+    document.querySelector('.footer').classList.toggle('footer-all-width');
 }
 
