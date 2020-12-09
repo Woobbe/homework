87bdedb744b4c7bc87f797853649eb99b8096a0a
Vitalii Shevtsov <woobbe@gmail.com>
2020-12-09 21:31:44 +0200

    Not for push!

--- js/HW_19/HW_19-infinity_slider.js
+++ js/HW_19/HW_19-infinity_slider.js
@@ -1,62 +1,69 @@
 const sliderBtnAside = document.querySelector('.aside__link-slider');
-const defaults = {
-    index: 0,
-    interval: 2000,
-    selectorWrapper: '.slider-container',
-    offset: 0
-};
 
-function startSlider() {
-    const sliderWrapper = document.querySelector(defaults.selectorWrapper);
-    const sliderWrapperWidth = sliderWrapper.offsetWidth;
-    const slidesList = getSlidesList(sliderWrapper);
-    let offset = 0;
-
-    createSlide(sliderWrapper, sliderWrapperWidth, slidesList, offset);
-    offset = 1;
-    createSlide(sliderWrapper, sliderWrapperWidth, slidesList, offset);
-
-    setInterval(moveSliderLeft, defaults.interval, sliderWrapper, sliderWrapperWidth, slidesList, offset);
-    sliderBtnAside.removeEventListener('click', startSlider);
-}
-
-function getSlidesList(sliderWrapper) {
-    const slides = sliderWrapper.querySelectorAll('img');
-    const slidesList = [];
-
-    for (let i = 0; i < slides.length; i++) {
-        slidesList[i] = slides[i].src;
-        slides[i].remove();
+class Slider {
+    constructor() {
+        this.index = 0;
+        this.interval = 500;
+        this.selectorWrapper = '.slider-container';
+        this.offset = 0;
+        this.sliderWrapper = document.querySelector(this.selectorWrapper);
+        // TODO: offsetWidth возвращает 0!!!
+        this.sliderWrapperWidth = this.sliderWrapper.offsetWidth;
+        this.slidesList = this.getSlidesList(this.sliderWrapper);
     }
-    return slidesList;
+
+    startSlider() {
+        console.log(this);
+        this.createSlide();
+        this.offset = 1;
+        this.createSlide();
+
+        setInterval(this.moveSliderLeft.bind(infinitySlider), this.interval);
+    }
+
+    getSlidesList() {
+        const slides = this.sliderWrapper.querySelectorAll('img');
+        const slidesList = [];
+
+        for (let i = 0; i < slides.length; i++) {
+            slidesList[i] = slides[i].src;
+            slides[i].remove();
+        }
+        return slidesList;
+    }
+
+    createSlide() {
+        const slide = document.createElement('img');
+        slide.className = 'slider__item';
+        slide.src = this.slidesList[this.index];
+        slide.style.left = this.offset * this.sliderWrapperWidth + 'px';
+        this.sliderWrapper.append(slide);
+
+        this.index + 1 === this.slidesList.length
+            ? this.index = 0
+            : ++this.index;
+    }
+
+    moveSliderLeft() {
+        const currentSlides = this.sliderWrapper.querySelectorAll('img');
+
+        currentSlides[0].style.left = -this.sliderWrapperWidth + 'px';
+        // Принудительно вызываем reflow!!!
+        currentSlides[1].offsetHeight;
+        currentSlides[1].style.left = '0px';
+
+        currentSlides[0].addEventListener('transitionend', () => {
+            currentSlides[0].remove();
+            this.createSlide.call(infinitySlider);
+        }, {once: true});
+    }
 }
 
-function createSlide(sliderWrapper, sliderWrapperWidth, slidesList, offset) {
-    const slide = document.createElement('img');
-    slide.className = 'slider__item';
-    slide.src = slidesList[defaults.index];
-    slide.style.left = offset * sliderWrapperWidth + 'px';
-    sliderWrapper.append(slide);
+const infinitySlider = new Slider();
 
-    defaults.index + 1 === slidesList.length
-        ? defaults.index = 0
-        : ++defaults.index;
-}
 
-function moveSliderLeft(sliderWrapper, sliderWrapperWidth, slidesList, offset) {
-    const currentSlides = sliderWrapper.querySelectorAll('img');
 
-    currentSlides[0].style.left = -sliderWrapperWidth + 'px';
-    // Принудительно вызываем reflow!!!
-    currentSlides[1].offsetHeight;
-    currentSlides[1].style.left = '0px';
 
-    currentSlides[0].addEventListener('transitionend', () => {
-        currentSlides[0].remove();
-        createSlide(sliderWrapper, sliderWrapperWidth, slidesList, offset);
-    }, {once: true});
-}
-
-sliderBtnAside.addEventListener('click', showSlider)
-sliderBtnAside.addEventListener('click', startSlider);
+sliderBtnAside.addEventListener('click', showSlider);
+sliderBtnAside.addEventListener('click', infinitySlider.startSlider.bind(infinitySlider), {once: true});
 
